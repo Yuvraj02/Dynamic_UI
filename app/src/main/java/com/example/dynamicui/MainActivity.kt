2ec3a6073ffd3438c0a652b4121944d7fe9a3d93
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2021-09-13 20:20:39 +0530

    Created MVVM structure,Live Data and added a runtime dependency

--- app/src/main/java/com/example/dynamicui/MainActivity.kt
+++ app/src/main/java/com/example/dynamicui/MainActivity.kt
@@ -3,22 +3,20 @@ package com.example.dynamicui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dynamicui.ui.theme.DynamicUITheme
 
-val list: ArrayList<String> = arrayListOf("John","Michael","Dana","Andrew")
+val list: ArrayList<String> = arrayListOf("John", "Michael", "Dana", "Andrew")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +25,7 @@ class MainActivity : ComponentActivity() {
             DynamicUITheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                   // Greeting("Android")
+                    // Greeting("Android")
                     MainScreen()
                 }
             }
@@ -36,24 +34,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(){
-    Column(modifier = Modifier.fillMaxSize(),verticalArrangement = Arrangement.SpaceEvenly,horizontalAlignment = Alignment.CenterHorizontally) {
-        GreetingList()
+fun MainScreen(viewModel:MainViewModel = MainViewModel()) {
+
+   // val greetingListState = remember { mutableStateListOf<String>("Yuvraj", "Nishchay") }
+    val newNameSetContent = viewModel.textFieldState.observeAsState("")
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        GreetingMessage(
+            newNameSetContent.value
+        ) { newName -> viewModel.onTextChanged(newName) }
     }
 }
 
 @Composable
-fun GreetingList(){
+fun GreetingMessage(
+    textFieldValue: String,
+    textFieldUpdate: (newName: String) -> Unit
+) {
 
-    val greetingListState = remember {mutableStateListOf<String>("Yuvraj","Nishchay")}
+//    for (name in namesList) {
+//        Text(text = "Hello $name", style = MaterialTheme.typography.h3)
+//    }
 
-    for (name in greetingListState){
-        Text(text = "Hello $name",style = MaterialTheme.typography.h3)
-    }
-    
-    Button(onClick = { greetingListState.add("Rashi")}) {
-        Text(text = "Add new name")
-        
+
+    TextField(
+        value = textFieldValue,
+        onValueChange = textFieldUpdate
+    )
+
+    Button(onClick = {}) {
+        Text(text = textFieldValue)
+
     }
 }
 
